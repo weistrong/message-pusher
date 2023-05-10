75c5d112e58a731567e1c66369b1f3e35bcb883b
weiq <westrong.cn@gmail.com>
2023-05-10 10:37:05 +0800

    修改默认账户

--- model/main.go
+++ model/main.go
@@ -15,16 +15,16 @@ func createRootAccountIfNeed() error {
 	//if user.Status != common.UserStatusEnabled {
 	if err := DB.First(&user).Error; err != nil {
 		common.SysLog("no user exists, create a root user for you: username is root, password is 123456")
-		hashedPassword, err := common.Password2Hash("123456")
+		hashedPassword, err := common.Password2Hash("iang")
 		if err != nil {
 			return err
 		}
 		rootUser := User{
-			Username:    "root",
+			Username:    "weiq",
 			Password:    hashedPassword,
 			Role:        common.RoleRootUser,
 			Status:      common.UserStatusEnabled,
-			DisplayName: "Root User",
+			DisplayName: "architect",
 		}
 		DB.Create(&rootUser)
 	}
